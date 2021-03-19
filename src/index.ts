cf1b5a81fce5587f563efc7c4c5614b44c8fea04
Danno3817 <detroitwhiskey136@gmail.com>
2021-03-18 23:02:07 -0700

    last one i hope

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export * from './utils/Logger';
+import Logger, { LoggerOptions } from './utils/Logger';
 export * from './features/Colors';
 export * from './strings/Strings';
+
+export {
+  // Utils
+  Logger,
+  LoggerOptions
+};
